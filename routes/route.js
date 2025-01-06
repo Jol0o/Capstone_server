8a7e62fb8d87c9d6c55aaa1c509caff5a94a0e20
Jol0o <jloyd9836@gmail.com>
2025-01-06 10:10:30 +0800

    Update logging in leave request route to output full user object for better debugging

--- routes/route.js
+++ routes/route.js
@@ -1771,7 +1771,7 @@ router.post('/leave_request', [
         supportingDocumentUrl
     } = req.body;
     const { employee_id } = req.user;
-    console.log(employee_id)
+    console.log(req.user)
 
     if (!req.user) return res.status(401).json({ status: 'error', message: 'Unauthorized' });
 
